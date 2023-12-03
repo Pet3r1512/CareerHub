f07921a13465ce96cd6986c2460307d49c566144
Thanh Phong <pttp15122002@gmail.com>
2023-12-03 17:11:06 +0700

    allow passing className to Logo

--- src/assets/_UI/_logo.tsx
+++ src/assets/_UI/_logo.tsx
@@ -1,6 +1,13 @@
-export default function Logo() {
+import { twMerge } from "tailwind-merge";
+
+export default function Logo({ className }: { className?: string }) {
   return (
-    <div className="font-mono font-extrabold text-3xl text-primary cursor-default">
+    <div
+      className={twMerge(
+        "font-mono font-extrabold text-3xl text-primary cursor-default",
+        className
+      )}
+    >
       JobParadise
     </div>
   );
