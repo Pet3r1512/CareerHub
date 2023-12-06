fd068155ac7f3e965761ec79588e127f4fb5ed5f
Thanh Phong <pttp15122002@gmail.com>
2023-12-06 18:40:20 +0700

    main content of hero

---
+++ src/assets/_UI/Hero/_title.tsx
@@ -0,0 +1,13 @@
+export default function Title() {
+  return (
+    <div className="md:w-[38%] flex flex-col gap-y-16">
+      <h1 className="md:text-[95px] md:leading-none font-bold">
+        Discover more than <span className="text-blue">5000+ Jobs</span>
+      </h1>
+      <p className="text-lg font-semibold text-gray">
+        Great platform for job seekers that passionate about startups. Find your
+        dream job easier in this paradise
+      </p>
+    </div>
+  );
+}
