9de5ffa727419f4c1ea30564662b1a6dccdf6b31
Thanh Phong <pttp15122002@gmail.com>
2023-12-06 18:39:55 +0700

    allow to pass new className

--- src/assets/_UI/Page.tsx
+++ src/assets/_UI/Page.tsx
@@ -1,20 +1,28 @@
 import Head from "next/head";
 import { ReactNode } from "react";
 import Footer from "./Footer";
+import { twMerge } from "tailwind-merge";
 
 export default function Page({
   children,
   pageName = "Home",
+  className,
 }: {
   children: ReactNode;
   pageName?: string;
+  className?: string;
 }) {
   return (
     <div>
       <Head>
         <title>JobParadise - {pageName}</title>
       </Head>
-      <main className="max-w-[100rem] mx-auto py-5 md:min-h-screen">
+      <main
+        className={twMerge(
+          "max-w-[100rem] mx-auto py-5 md:min-h-screen",
+          className
+        )}
+      >
         {children}
       </main>
       <Footer />
