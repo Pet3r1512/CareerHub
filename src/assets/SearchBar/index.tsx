fd2fb790d09449e7056c5c5f7fde15ceadb5fede
Thanh Phong <pttp15122002@gmail.com>
2023-12-18 22:29:15 +0700

    completed search bar in hero section

--- src/assets/SearchBar/index.tsx
+++ src/assets/SearchBar/index.tsx
@@ -1,9 +1,13 @@
-import Write from "./_write";
+import ButtonBlock from "../_UI/_button";
+import FindJobLocation from "./_findJobLocation";
+import FindJobName from "./_findJobName";
 
 export default function SearchBar() {
   return (
-    <>
-      <Write />
-    </>
+    <div className="flex mx-4 sm:mx-0 lg:items-center gap-y-4 sm:gap-y-2 gap-x-2 flex-col lg:flex-row relative">
+      <FindJobName />
+      <FindJobLocation />
+      <ButtonBlock content="Search" />
+    </div>
   );
 }
