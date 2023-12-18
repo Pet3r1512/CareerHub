4f46c28fc8332bee6be980905e6aaa99887e3278
Thanh Phong <pttp15122002@gmail.com>
2023-12-18 22:28:40 +0700

    rename

--- src/assets/SearchBar/_findJobName.tsx
+++ src/assets/SearchBar/_findJobName.tsx
@@ -1,11 +1,16 @@
 import { Input } from "@material-tailwind/react";
 import { Search } from "lucide-react";
 
-export default function Write() {
+export default function FindJobName() {
   return (
     <div className="flex items-end gap-x-2">
       <Search className="mb-1" />
-      <Input variant="standard" className="!text-xl" crossOrigin={true} />
+      <Input
+        label="Find Job Name"
+        variant="standard"
+        className="!text-lg"
+        crossOrigin={true}
+      />
     </div>
   );
 }
