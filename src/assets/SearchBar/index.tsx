6a92b5f2cf163b09ff2e03fe05548a48ae48019e
Thanh Phong <pttp15122002@gmail.com>
2023-12-24 20:44:11 +0700

    fix button clicked animation

--- src/assets/SearchBar/index.tsx
+++ src/assets/SearchBar/index.tsx
@@ -4,7 +4,7 @@ import FindJobName from "./_findJobName";
 
 export default function SearchBar() {
   return (
-    <div className="flex mx-4 sm:mx-0 lg:items-center gap-y-4 sm:gap-y-2 gap-x-2 flex-col lg:flex-row relative">
+    <div className="w-fit flex mx-4 sm:mx-0 lg:items-center gap-y-4 sm:gap-y-2 gap-x-2 flex-col lg:flex-row relative">
       <FindJobName />
       <FindJobLocation />
       <ButtonBlock content="Search" />
