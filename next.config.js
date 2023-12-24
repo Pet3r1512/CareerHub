9f56c933eeb29da504c049fdfe4c88c16019563e
Thanh Phong <pttp15122002@gmail.com>
2023-12-24 20:32:49 +0700

    config supabase in nextjs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "jlehnhviqykpbhjqjzmp.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/sign/Static%20Images/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
