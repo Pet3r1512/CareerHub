b324b18a7a70d46c90d1e535079c585da89ad6ee
Thanh Phong <pttp15122002@gmail.com>
2024-01-01 21:06:19 +0700

    use Sponsor

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,13 +2,15 @@ import ButtonBlock from "@/assets/_UI/_button";
 import Header from "@/assets/_UI/Header";
 import Hero from "@/assets/_UI/Hero";
 import Page from "@/assets/_UI/Page";
+import Sponsor from "@/assets/_UI/Sponsor";
 
 export default function Home() {
   return (
     <Page className="flex flex-col gap-y-4 md:gap-y-24">
       <Header />
-      <section className="lg:min-h-screen">
+      <section className="lg:min-h-screen lg:pb-16 relative">
         <Hero />
+        <Sponsor />
       </section>
     </Page>
   );
