74ac8d2c15ca63d235dc40e8309cd6201e60d1d6
Thanh Phong <pttp15122002@gmail.com>
2024-01-04 20:50:07 +0700

    responsive for mobiles

--- src/assets/home/Category/CategoryContainer.tsx
+++ src/assets/home/Category/CategoryContainer.tsx
@@ -10,14 +10,14 @@ export default function CategoryContainer({
   avalibleJobs: number;
 }) {
   return (
-    <div className="border-2 w-full group border-gray-light hover:border-primary hover:bg-primary hover:text-white duration-150 transition-all ease-linear px-8 py-6 rounded-xl">
-      <p className="text-xl text-primary group-hover:text-white transition-all duration-150 ease-linear">
+    <div className="border-2 w-full group border-gray-light lg:hover:border-primary lg:hover:bg-primary lg:hover:text-white duration-150 transition-all ease-linear p-4 py-6 lg:px-8 lg:py-6 rounded-xl flex flex-col justify-between">
+      <p className="text-xl text-primary lg:group-hover:text-white transition-all duration-150 ease-linear">
         {icon}
       </p>
-      <p className="font-bold text-lg mt-5 group-hover:text-xl transition-all duration-150 ease-linear">
+      <p className="font-bold text-lg mt-5 lg:group-hover:text-xl transition-all duration-150 ease-linear">
         {categoryName}
       </p>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 justify-between">
         {avalibleJobs} jobs avalible
         <MoveRight />
       </div>
