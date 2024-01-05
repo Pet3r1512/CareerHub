6bffa15fa6a90adef8d08ba43b9242e104233dd2
Thanh Phong <pttp15122002@gmail.com>
2024-01-05 10:14:06 +0700

    use sidebar

--- src/assets/_UI/Header/index.tsx
+++ src/assets/_UI/Header/index.tsx
@@ -1,21 +1,30 @@
+import { ChevronLeft } from "lucide-react";
 import ButtonBlock from "../_button";
 import Logo from "../_logo";
 import Menu from "./_menu";
+import { useState } from "react";
+import Sidebar from "./_sidebar";
+import Auth from "./_auth";
 
 export default function Header() {
+  const [openSidebar, setOpenSidebar] = useState(false);
+
   return (
     <section className="w-full flex justify-between items-center">
       <div className="flex items-center gap-8">
         <Logo />
         <Menu />
       </div>
-      <div className="items-center gap-5 hidden lg:flex">
-        <ButtonBlock
-          className="bg-white text-primary font-bold"
-          content="Login"
-        />
-        <ButtonBlock content="Sign Up" />
-      </div>
+      <button
+        className="flex items-center lg:hidden font-semibold"
+        onClick={() => {
+          setOpenSidebar(true);
+        }}
+      >
+        Menu <ChevronLeft />
+      </button>
+      <Sidebar isOpen={openSidebar} setIsOpen={setOpenSidebar} />
+      <Auth className="hidden lg:flex" />
     </section>
   );
 }
