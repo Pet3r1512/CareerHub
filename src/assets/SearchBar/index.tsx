fa18685e530e2024ccb48ec51b2904ece9304739
Thanh Phong <pttp15122002@gmail.com>
2024-01-05 10:21:13 +0700

    expand block's width on mobiles

--- src/assets/SearchBar/index.tsx
+++ src/assets/SearchBar/index.tsx
@@ -4,7 +4,7 @@ import FindJobName from "./_findJobName";
 
 export default function SearchBar() {
   return (
-    <div className="w-fit mx-auto flex sm:mx-0 lg:items-center gap-y-4 sm:gap-y-2 gap-x-2 flex-col lg:flex-row relative">
+    <div className="lg:w-fit px-6 lg:px-0 flex sm:mx-0 lg:items-center gap-y-4 sm:gap-y-2 gap-x-2 flex-col lg:flex-row relative">
       <FindJobName />
       <FindJobLocation />
       <ButtonBlock content="Search" />
