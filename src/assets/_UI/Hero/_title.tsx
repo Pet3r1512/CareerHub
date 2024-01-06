d54135e5c61e9844ea96202fb0e1650cd7e71ac3
Thanh Phong <pttp15122002@gmail.com>
2024-01-06 09:53:44 +0700

    remove image on mobile, update content and UI

--- src/assets/_UI/Hero/_title.tsx
+++ src/assets/_UI/Hero/_title.tsx
@@ -4,22 +4,13 @@ import Image from "next/image";
 export default function Title() {
   return (
     <div className="md:w-[38%] flex flex-col gap-y-4 md:gap-y-8 lg:gap-y-16">
-      <div className="relative">
-        <Image
-          src="https://jlehnhviqykpbhjqjzmp.supabase.co/storage/v1/object/sign/Static%20Images/Hero.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJTdGF0aWMgSW1hZ2VzL0hlcm8ucG5nIiwiaWF0IjoxNzAyOTk3Mjc1LCJleHAiOjE3MzQ1MzMyNzV9.p1aKs31GREmrubUD2oE1AMmGDNpGRmh0YeuwdBfMmlo&t=2023-12-19T14%3A47%3A44.808Z"
-          alt=""
-          // width={731.6} height={710.4}
-          fill
-          sizes="100"
-          className="md:hidden !h-fit !w-full !mx-auto !relative"
-        />
-      </div>
-      <h1 className="text-2xl md:text-6xl lg:text-[90px] md:leading-none font-bold">
-        Discover more than <span className="text-blue">5000+ Jobs</span>
+      <h1 className="text-[70px] leading-none md:text-6xl lg:text-[90px] md:leading-none font-bold">
+        Discover more than{" "}
+        <span className="text-blue leading-tight">5000+ Jobs</span>
       </h1>
       <p className="md:text-md lg:text-lg font-semibold text-gray">
-        Great platform for job seekers that passionate about startups. Find your
-        dream job easier in this paradise
+        An excellent platform for individuals enthusiastic about startups.
+        Discover your ideal job more effortlessly in this haven.
       </p>
       <SearchBar />
     </div>
