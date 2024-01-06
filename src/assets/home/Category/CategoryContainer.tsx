a2472099cfef33da44b4188d944ad8648029506d
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-01-06 10:06:53 +0700

    Update mobiles UI (#7)
    * remove image on mobile, update content and UI
    
    * responsive for mobiles
    
    * update color for input
    
    * prevent auto zoom to Text input

--- src/assets/home/Category/CategoryContainer.tsx
+++ src/assets/home/Category/CategoryContainer.tsx
@@ -11,13 +11,13 @@ export default function CategoryContainer({
 }) {
   return (
     <div className="border-2 w-full group border-gray-light lg:hover:border-primary lg:hover:bg-primary lg:hover:text-white duration-150 transition-all ease-linear p-4 py-6 lg:px-8 lg:py-6 rounded-xl flex flex-col justify-between">
-      <p className="text-xl text-primary lg:group-hover:text-white transition-all duration-150 ease-linear">
+      <p className="text-2xl text-primary lg:group-hover:text-white transition-all duration-150 ease-linear">
         {icon}
       </p>
-      <p className="font-bold text-lg mt-5 lg:group-hover:text-xl transition-all duration-150 ease-linear">
+      <p className="font-bold text-2xl md:text-lg mt-5 lg:group-hover:text-xl transition-all duration-150 ease-linear">
         {categoryName}
       </p>
-      <div className="flex items-center gap-4 justify-between">
+      <div className="flex text-lg md:text-md items-center gap-4 justify-between">
         {avalibleJobs} jobs avalible
         <MoveRight />
       </div>
--- src/assets/home/Category/index.tsx
+++ src/assets/home/Category/index.tsx
@@ -15,58 +15,58 @@ export default function Category() {
   const sampleCategoryData = [
     {
       id: 1,
-      icon: <PencilRuler />,
+      icon: <PencilRuler size={32} />,
       name: "Design",
       jobs: 235,
     },
     {
       id: 2,
-      icon: <BarChart2 />,
+      icon: <BarChart2 size={32} />,
       name: "Sales",
       jobs: 756,
     },
     {
       id: 3,
-      icon: <BadgeDollarSign />,
+      icon: <BadgeDollarSign size={32} />,
       name: "Marketing",
       jobs: 140,
     },
     {
       id: 4,
-      icon: <Banknote />,
+      icon: <Banknote size={32} />,
       name: "Finace",
       jobs: 90,
     },
     {
       id: 5,
-      icon: <Code />,
+      icon: <Code size={32} />,
       name: "Engineering",
       jobs: 540,
     },
     {
       id: 6,
-      icon: <PersonStanding />,
+      icon: <PersonStanding size={32} />,
       name: "Human Resources",
       jobs: 210,
     },
     {
       id: 7,
-      icon: <Router />,
+      icon: <Router size={32} />,
       name: "Technology",
       jobs: 112,
     },
     {
       id: 8,
-      icon: <BookOpen />,
+      icon: <BookOpen size={32} />,
       name: "Education",
       jobs: 310,
     },
   ];
 
   return (
-    <section className="flex flex-col gap-y-16 mt-16 lg:mt-0">
-      <div className="flex items-center justify-between">
-        <h1 className="font-extrabold text-2xl sm:text-3xl lg:text-5xl">
+    <section className="flex flex-col gap-y-8 md:gap-y-16 mt-16 lg:mt-0">
+      <div className="flex flex-col md:flex-row md:items-center md:justify-between gap-y-4">
+        <h1 className="font-extrabold text-5xl sm:text-4xl lg:text-5xl">
           Explore by <span className="text-blue">category</span>
         </h1>
         <div className="flex items-center gap-x-2 text-primary">
@@ -76,7 +76,7 @@ export default function Category() {
           <MoveRight />
         </div>
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-3 justify-items-center lg:grid-cols-4 w-full lg:grid-rows-2 grid-flow-row auto-rows-auto lg:gap-8 gap-2 sm:gap-4">
+      <div className="grid grid-cols-1 px-6 md:px-0 sm:grid-cols-3 justify-items-center lg:grid-cols-4 w-full lg:grid-rows-2 grid-flow-row auto-rows-auto lg:gap-8 gap-4 sm:gap-4">
         {/* Containers here */}
         {sampleCategoryData.map((category) => {
           return (
