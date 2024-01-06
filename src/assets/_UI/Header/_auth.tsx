437408fa11d27b97ad7fee3dcc005c19099e15d9
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-01-06 18:50:39 +0700

    Build signup page (#9)
    * set cursor to default, preventing text copy
    
    * build signup page
    
    * add navigate link
    
    * add back home button

--- src/assets/_UI/Header/_auth.tsx
+++ src/assets/_UI/Header/_auth.tsx
@@ -1,14 +1,19 @@
 import { twMerge } from "tailwind-merge";
 import ButtonBlock from "../_button";
+import Link from "next/link";
 
 export default function Auth({ className }: { className?: string }) {
   return (
     <div className={twMerge("items-center gap-5 flex", className)}>
-      <ButtonBlock
-        className="bg-white text-primary font-bold"
-        content="Login"
-      />
-      <ButtonBlock content="Sign Up" />
+      <Link href="/auth/signin">
+        <ButtonBlock
+          className="bg-white text-primary font-bold"
+          content="Login"
+        />
+      </Link>
+      <Link href={"/auth/signup"}>
+        <ButtonBlock content="Sign Up" />
+      </Link>
     </div>
   );
 }
--- src/assets/_UI/_logo.tsx
+++ src/assets/_UI/_logo.tsx
@@ -2,7 +2,9 @@ import { twMerge } from "tailwind-merge";
 
 export default function Logo({ className }: { className?: string }) {
   return (
-    <div className={twMerge("flex items-center gap-x-2", className)}>
+    <div
+      className={twMerge("flex items-center gap-x-2 cursor-default", className)}
+    >
       <img
         src="https://jlehnhviqykpbhjqjzmp.supabase.co/storage/v1/object/sign/Static%20Images/CareerHubLogo.png?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJTdGF0aWMgSW1hZ2VzL0NhcmVlckh1YkxvZ28ucG5nIiwiaWF0IjoxNzA0NDIwNTc5LCJleHAiOjE3MzU5NTY1Nzl9.KRIc3FWlTw2qUVGoHfpL1OjUND5BTIjcn6QYUfmKL-s&t=2024-01-05T02%3A09%3A38.278Z"
         alt=""
