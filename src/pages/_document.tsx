93368f23ef9891618863c9d3062d890477ba469d
Thanh Phong <pttp15122002@gmail.com>
2024-01-08 08:52:44 +0700

    enable Meticulous AI testing

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,15 @@ export default function Document() {
         name="viewport"
         content="width=device-width, initial-scale=1, maximum-scale=1"
       ></meta>
+      {(process.env.NODE_ENV === "development" ||
+        process.env.VERCEL_ENV === "preview") && (
+        // eslint-disable-next-line @next/next/no-sync-scripts
+        <script
+          data-project-id="LlgWSBQE9owFvhO4jb5yM1bsCBd7WbymV8q7BLBJ"
+          data-is-production-environment="false"
+          src="https://snippet.meticulous.ai/v1/meticulous.js"
+        />
+      )}
       <body>
         <Main />
         <NextScript />
