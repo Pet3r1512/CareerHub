159a4e02d19bec5cc481171685648683e5e646bb
LeKhoi <ledinhkhoimd@gmail.com>
2024-01-08 11:56:02 +0700

    Add and config shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,86 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 import withMT from "@material-tailwind/react/utils/withMT";
 
-const config: Config = {
-  darkMode: 'class',
+const config = {
+  darkMode: ["class"],
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/assets/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/assets/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  prefix: "",
   theme: {
-    colors: {
-      primary: "#4640DE",
-      blue: "#26A4FF",
-      darkBlue: "#202430",
-      green: "#56CDAD",
-      white: "#FFF",
-      black: "#000",
-      gray: {
-        dark: "#626B7D",
-        light: "#ced4da"
-      }
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
     },
-    extend: {},
+    extend: {
+      colors: {
+        border: "hsl(var(--border))",
+        input: "hsl(var(--input))",
+        ring: "hsl(var(--ring))",
+        background: "hsl(var(--background))",
+        foreground: "hsl(var(--foreground))",
+        primary: "#4640DE",
+        blue: "#26A4FF",
+        darkBlue: "#202430",
+        green: "#56CDAD",
+        white: "#FFF",
+        black: "#000",
+        gray: {
+          dark: "#626B7D",
+          light: "#ced4da",
+        },
+        secondary: {
+          DEFAULT: "hsl(var(--secondary))",
+          foreground: "hsl(var(--secondary-foreground))",
+        },
+        destructive: {
+          DEFAULT: "hsl(var(--destructive))",
+          foreground: "hsl(var(--destructive-foreground))",
+        },
+        muted: {
+          DEFAULT: "hsl(var(--muted))",
+          foreground: "hsl(var(--muted-foreground))",
+        },
+        accent: {
+          DEFAULT: "hsl(var(--accent))",
+          foreground: "hsl(var(--accent-foreground))",
+        },
+        popover: {
+          DEFAULT: "hsl(var(--popover))",
+          foreground: "hsl(var(--popover-foreground))",
+        },
+        card: {
+          DEFAULT: "hsl(var(--card))",
+          foreground: "hsl(var(--card-foreground))",
+        },
+      },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
   },
-  plugins: [],
-}
-export default withMT(config)
+  plugins: [require("tailwindcss-animate"), require("@tailwindcss/line-clamp")],
+} satisfies Config;
+
+export default withMT(config);
