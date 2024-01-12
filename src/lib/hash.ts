aad0ca4cbd666c2e4866d435725147c49b7d532a
Thanh Phong <pttp15122002@gmail.com>
2024-01-12 20:16:22 +0700

    upload missing file

---
+++ src/lib/hash.ts
@@ -0,0 +1,10 @@
+import * as bcrypt from 'bcrypt';
+
+async function hashPassword(password: string): Promise<string> {
+  const saltRounds = 10;
+  const salt = await bcrypt.genSalt(saltRounds);
+  const hash = await bcrypt.hash(password, salt);
+  return hash;
+}
+
+export default hashPassword
