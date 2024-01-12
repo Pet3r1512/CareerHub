fe2ac90cb2e36816fd3c772905982899455ebf6f
LeKhoi <ledinhkhoimd@gmail.com>
2024-01-12 18:22:18 +0700

    Create browse companies page

--- src/pages/companies.tsx
+++ src/pages/companies.tsx
@@ -5,20 +5,17 @@ import Companies from "@/assets/Companies";
 
 export default function CompaniesPage() {
   return (
-    <main className="max-w-screen mx-auto pt-6 px-0 lg:min-h-screen">
+    <main className="max-w-screen pt-6 px-0 lg:min-h-screen">
       <section className="max-w-[100rem] mx-auto px-2 lg:px-0">
         <Header />
       </section>
-      <section className="lg:max-h-screen w-full">
-        <Intro
-          title="dream companies"
-          description="Find the dream companies you dream work for"
-          searchType="company"
-        />
-      </section>
-      <section className="lg:min-h-screen w-full">
-        <Companies />
-      </section>
+      <Intro
+        title="dream companies"
+        description="Find the dream companies you dream work for"
+        searchType="company"
+        className="mt-8 lg:max-h-screen w-full"
+      />
+      <Companies />
       <Footer />
     </main>
   );
