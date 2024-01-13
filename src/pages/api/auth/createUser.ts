16d2792daf0be2c6fa7ecc4df524c53eaffdd605
Thanh Phong <pttp15122002@gmail.com>
2024-01-13 22:17:50 +0700

    refactor

--- src/pages/api/auth/createUser.ts
+++ src/pages/api/auth/createUser.ts
@@ -6,19 +6,40 @@ const prisma = new PrismaClient()
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     const parsedBody = JSON.parse(req.body)
     const {full_name, gender, email, password} = parsedBody.values
-    const testRecord = await prisma.user.findFirst({
-        where: {OR: [{full_name},{email}]}
-       });
-    if (testRecord) {
-        return res.status(409).json({result: "Conflict","message": "Email address already exists. Please try again."})
-    }
-    await prisma.user.create({
-        data: {
+    try {
+        const testRecord = await prisma.user.findFirst({
+          where: { OR: [{ full_name }, { email }] }
+        });
+      
+        if (testRecord) {
+          return res.status(409).json({
+            result: "Conflict",
+            message: "Email address or full name already exists. Please try again."
+          });
+        }
+      
+        await prisma.user.create({
+          data: {
             full_name: full_name,
             gender: gender,
             email: email,
             password: password
-        }
-    })
-    return res.status(201).json({result: "Done",message: "Create New User Successfully!"})
+          }
+        });
+      
+        return res.status(201).json({
+          result: "Done",
+          message: "Create New User Successfully!"
+        });
+      } catch (error) {
+        console.error("Error creating user:", error);
+        return res.status(500).json({
+          result: "Error",
+          message: "Internal Server Error. Please try again later."
+        });
+      } finally {
+        // Make sure to disconnect from the Prisma client after using it
+        await prisma.$disconnect();
+      }
+      
 }
