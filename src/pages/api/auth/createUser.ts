c956567fd0f0b21052da18babae7b25f6439414c
Thanh Phong <pttp15122002@gmail.com>
2024-01-19 19:12:36 +0700

    use middleware

--- src/pages/api/auth/createUser.ts
+++ src/pages/api/auth/createUser.ts
@@ -1,6 +1,41 @@
-import { NextApiRequest, NextApiResponse } from "next";
+import type { NextApiRequest, NextApiResponse } from "next";
+import Cors from "cors";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ["POST", "GET", "HEAD"],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function,
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient()
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   const parsedBody = JSON.parse(req.body)
   const { full_name, gender, email, password } = parsedBody.values
   try {
@@ -38,5 +73,4 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // Make sure to disconnect from the Prisma client after using it
     await prisma.$disconnect();
   }
-
 }
