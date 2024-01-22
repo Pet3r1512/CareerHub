2429ff76b7bd016ca626eabd25ac982c5542ed56
LeKhoi <ledinhkhoimd@gmail.com>
2024-01-22 11:15:53 +0700

    Push search result to url query

--- src/assets/SearchBar2/index.tsx
+++ src/assets/SearchBar2/index.tsx
@@ -10,12 +10,15 @@ import {
 import { Separator } from "@/components/ui/separator";
 import { Input } from "@/components/ui/input";
 import { SelectGroup } from "@radix-ui/react-select";
+import { useEffect, useState } from "react";
+import { useDebounce } from "@/hooks/useDebounce";
+import router from "next/router";
 
 type SearchBar2Props = {
   type: "company" | "job";
 };
 
-function FindJobLocation() {
+function ChooseLocation() {
   return (
     <div className="p-2 w-full lg:w-[40%] h-full flex gap-4 items-center group/item">
       <MapPin size={24} color="#424242" />
@@ -52,23 +55,36 @@ function FindJobLocation() {
   );
 }
 
-function FindJobName() {
-  return (
-    <div className="w-full p-2 flex gap-4 items-center h-10 lg:w-[40%] group/item">
-      <Search size={24} color="#424242" />
-      <div className="relative w-full">
-        <span className="absolute h-[1px] bg-black/20 w-full -bottom-3 left-0 lg:group-hover/item:bg-black/50 transition duration-300"></span>
-        <Input
-          type="text"
-          placeholder="Job title or keyword"
-          className="rounded-none border-none focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-600 p-0 h-fit w-full lg:text-base lg:placeholder:text-base"
-        />
-      </div>
-    </div>
-  );
-}
+function SearchName({ type }: SearchBar2Props) {
+  const [inputValue, setInputValue] = useState("");
+  const debouncedInputValue = useDebounce(inputValue, 500);
 
-function FindCompanyName() {
+  useEffect(() => {
+    if (type == "company") {
+      router.push(
+        {
+          pathname: "/companies",
+          query: { search: debouncedInputValue },
+        },
+        undefined,
+        { scroll: false }
+      );
+    } else {
+      router.push(
+        {
+          pathname: "/jobs",
+          query: { search: debouncedInputValue },
+        },
+        undefined,
+        { scroll: false }
+      );
+    }
+  }, [debouncedInputValue, type]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+  };
+
   return (
     <div className="w-full p-2 flex gap-4 items-center h-10 lg:w-[40%] group/item">
       <Search size={24} color="#424242" />
@@ -76,8 +92,14 @@ function FindCompanyName() {
         <span className="absolute h-[1px] bg-black/20 w-full -bottom-3 left-0 lg:group-hover/item:bg-black/50 transition duration-300"></span>
         <Input
           type="text"
-          placeholder="Company name or keyword"
+          placeholder={
+            type == "company"
+              ? "Company name or keyword"
+              : "Job title or keyword"
+          }
           className="rounded-none border-none focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-gray-600 p-0 h-fit w-full lg:text-base lg:placeholder:text-base"
+          onChange={handleInputChange}
+          value={inputValue}
         />
       </div>
     </div>
@@ -87,12 +109,12 @@ function FindCompanyName() {
 export default function SearchBar2({ type }: SearchBar2Props) {
   return (
     <div className="flex flex-wrap gap-8 lg:gap-0 p-8 px-4 justify-evenly items-center w-full bg-white">
-      {type === "company" ? <FindCompanyName /> : <FindJobName />}
+      <SearchName type={type} />
       <Separator
         orientation="vertical"
         className="bg-gray-light lg:h-10 hidden lg:block"
       />
-      <FindJobLocation />
+      <ChooseLocation />
       <ButtonBlock content="Search" />
     </div>
   );
