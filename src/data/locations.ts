e2534063fbb0f16283f70c8e9a16cfbae63913d4
LeKhoi <ledinhkhoimd@gmail.com>
2024-01-22 13:43:51 +0700

    Create location data

---
+++ src/data/locations.ts
@@ -0,0 +1,22 @@
+export const locations = [
+  {
+    name: "Ho Chi Minh city",
+    value: "SG",
+  },
+  {
+    name: "Ha Noi",
+    value: "HN",
+  },
+  {
+    name: "Da Nang",
+    value: "DN",
+  },
+  {
+    name: "Can Tho",
+    value: "CT",
+  },
+  {
+    name: "Binh Duong",
+    value: "BD",
+  },
+];
