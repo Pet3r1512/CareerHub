9d905ec3115f583ec42ad0e0b046cf891158232b
Thanh Phong <pttp15122002@gmail.com>
2024-01-22 17:50:37 +0700

    user's profile page

---
+++ src/pages/profile.tsx
@@ -0,0 +1,30 @@
+// pages/index.tsx
+
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+import Cookies from "js-cookie";
+
+function Profile() {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!Cookies.get("token")) {
+      router.push("/auth/signin");
+    }
+  }, []);
+  return (
+    <div>
+      <p>Check the console for cookie information.</p>
+      <button
+        onClick={() => {
+          Cookies.remove("token");
+          router.push("/auth/signin");
+        }}
+      >
+        Sign Out
+      </button>
+    </div>
+  );
+}
+
+export default Profile;
