fdcc1f3e8ebe608ce1172acd42db07fb9b6b41dd
LeKhoi <ledinhkhoimd@gmail.com>
2024-01-22 13:47:45 +0700

    Create PushQuery function

---
+++ src/utils/routerQuery.tsx
@@ -0,0 +1,27 @@
+import Router from "next/router";
+
+type QueryProps = {
+  pathname: string;
+  query: {
+    search?: string | string[];
+    page?: string | string[] | number;
+    location?: string | string[];
+    options?: string | string[];
+  };
+};
+
+export default function PushQuery({ pathname, query }: QueryProps) {
+  return Router.push(
+    {
+      pathname: pathname,
+      query: {
+        search: query.search,
+        location: query.location,
+        options: query.options,
+        page: query.page,
+      },
+    },
+    undefined,
+    { scroll: false }
+  );
+}
