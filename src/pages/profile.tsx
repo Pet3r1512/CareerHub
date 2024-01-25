daceeb60ccdfaab3d36506d43e9e3e049e96b970
Thanh Phong <pttp15122002@gmail.com>
2024-01-25 21:07:16 +0700

    add Header, redirect if not logged in

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,25 +1,23 @@
 import React, { useEffect } from "react";
-import { useRouter } from "next/router";
 import Page from "@/assets/_UI/Page";
+import Header from "@/assets/_UI/Header";
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
 import { useAppSelector } from "@/lib/store";
 
 function Profile() {
   const router = useRouter();
-
-  const isAuthenticated = useAppSelector((state) => state.auth.isAuthenticated);
   const user = useAppSelector((state) => state.auth.user);
 
-  // Add routing for unAuthenticated user
-
   useEffect(() => {
-    if (!user) {
+    if (!Cookies.get().token || !user) {
       router.push("/auth/signin");
     }
   }, []);
 
   return (
     <Page pageName="Careerhub - Profile">
-      <p>{user?.full_name}</p>
+      <Header />
     </Page>
   );
 }
