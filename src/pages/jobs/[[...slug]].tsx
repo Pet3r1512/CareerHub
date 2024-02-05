80593e3aa98120216f34e3dfb46181d8d5005f27
LeKhoi <ledinhkhoimd@gmail.com>
2024-02-05 09:56:21 +0700

    Create job page route

---
+++ src/pages/jobs/[[...slug]].tsx
@@ -0,0 +1,19 @@
+import Page from "@/assets/_UI/Page";
+import Intro from "@/assets/Companies/intro";
+import { useState } from "react";
+
+export default function JobsPage() {
+  const [isSearchLoading, setIsSearchLoading] = useState(false);
+
+  return (
+    <Page pageName="Jobs">
+      <Intro
+        title="dream jobs"
+        description="Find your next career at companies like HubSpot, Nike, and Dropbox."
+        searchType="job"
+        className="mt-8 lg:max-h-screen w-full"
+        loading={{ isSearchLoading, setIsSearchLoading }}
+      />
+    </Page>
+  );
+}
