52a9510deef60d768ebb63813a90a2f00d845a6d
Thanh Phong <pttp15122002@gmail.com>
2024-02-06 08:09:19 +0700

    remove unused

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,17 +1,12 @@
 import React, { useEffect } from "react";
 import Page from "@/assets/_UI/Page";
-import Cookies from "js-cookie";
 import { useRouter } from "next/router";
 import UserDashboard from "@/assets/User/Dashboard";
 
 export default function Profile() {
   const router = useRouter();
 
-  useEffect(() => {
-    if (!Cookies.get().token) {
-      router.push("/auth/signin");
-    }
-  }, []);
+  useEffect(() => {}, []);
 
   return (
     <Page pageName="Profile" noMenu>
