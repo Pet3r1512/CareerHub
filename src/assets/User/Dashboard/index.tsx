8eb16d5ad59f5a494912a61347a1985f82427949
Thanh Phong <pttp15122002@gmail.com>
2024-02-07 15:59:19 +0700

    user dashboard page layout

--- src/assets/User/Dashboard/index.tsx
+++ src/assets/User/Dashboard/index.tsx
@@ -1,5 +1,12 @@
+import UserDashboardMenu from "../Menu";
+
 export default function UserDashboard() {
-  return <></>;
+  return (
+    <div className="flex pt-8 h-full">
+      <UserDashboardMenu />
+      <div className="flex-1"></div>
+    </div>
+  );
 }
 
 // Overview, Applied jobs, favorite jobs, job alerts
