260be4caaee1160e24401a861ef60bef1242dcaf
Thanh Phong <pttp15122002@gmail.com>
2024-02-10 10:50:00 +0700

    clear localstorage after 2 days

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,21 @@ import type { AppProps } from "next/app";
 import { ReduxProvider, store } from "@/lib/store";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const hours: number = 48;
+  const now: number = new Date().getTime();
+  const setupTime: string | null = localStorage.getItem("setupTime");
+
+  if (setupTime === null) {
+    localStorage.setItem("setupTime", now.toString());
+  } else {
+    const setupTimeNumber: number = parseInt(setupTime, 10);
+    if (now - setupTimeNumber > hours * 60 * 60 * 1000) {
+      localStorage.removeItem("user");
+      localStorage.removeItem("token");
+      localStorage.setItem("setupTime", now.toString());
+    }
+  }
+
   return (
     <ReduxProvider store={store}>
       <Component {...pageProps} />
