62919d4577b83f0cede7d246e7e89cba4ad7d7a7
Thanh Phong <pttp15122002@gmail.com>
2024-02-10 10:31:14 +0700

    render components base on selected section

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,18 +1,32 @@
 import React, { useEffect } from "react";
 import Page from "@/assets/_UI/Page";
-import { useRouter } from "next/router";
 import UserDashboardMenu from "@/assets/User/Menu";
+import Information from "@/assets/User/Dashboard/Information";
+import { usePathname, useSearchParams } from "next/navigation";
+import AccountSetting from "@/assets/User/Dashboard/AccountSetting";
+import Resume from "@/assets/User/Dashboard/Resume";
+import { useRouter } from "next/router";
 
 export default function Profile() {
   const router = useRouter();
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    router.push(pathname + "?section=public_profile");
+  }, []);
 
   return (
     <Page pageName="Profile" noMenu noFooter className="h-screen">
-      <div className="flex pt-8 h-full">
+      <div className="flex pt-8 h-full gap-2">
         <UserDashboardMenu />
-        <div className="flex-1"></div>
+        <div className="flex-1 shadow-2xl rounded-2xl">
+          {searchParams.get("section") === "public_profile" && <Information />}
+          {searchParams.get("section") === "account_settings" && (
+            <AccountSetting />
+          )}
+          {searchParams.get("section") === "your_resumes" && <Resume />}
+        </div>
       </div>
     </Page>
   );
