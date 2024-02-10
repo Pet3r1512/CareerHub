911e7ca154258fe87bb49e844ab4e2e40e4d0ed8
Thanh Phong <pttp15122002@gmail.com>
2024-02-10 10:30:54 +0700

    default layouts

---
+++ src/assets/User/Dashboard/Resume/index.tsx
@@ -0,0 +1,9 @@
+import UserDashboardSectionLayout from "../_UserDashboardSectionLayout";
+
+export default function Resume() {
+  return (
+    <UserDashboardSectionLayout sectionTitle="Your Resumes">
+      <></>
+    </UserDashboardSectionLayout>
+  );
+}
