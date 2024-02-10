d42c46b62d0f9e2d15a84ca94be9a0c6051c6cf7
Thanh Phong <pttp15122002@gmail.com>
2024-02-10 10:42:03 +0700

    add keys

--- src/pages/user/profile.tsx
+++ src/pages/user/profile.tsx
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import Page from "@/assets/_UI/Page";
 import UserDashboardMenu from "@/assets/User/Menu";
-import Information from "@/assets/User/Dashboard/Information";
 import { usePathname, useSearchParams } from "next/navigation";
 import AccountSetting from "@/assets/User/Dashboard/AccountSetting";
 import Resume from "@/assets/User/Dashboard/Resume";
 import { useRouter } from "next/router";
+import Information from "@/assets/User/Dashboard/Information";
 
 export default function Profile() {
   const router = useRouter();
@@ -21,11 +21,15 @@ export default function Profile() {
       <div className="flex pt-8 h-full gap-2">
         <UserDashboardMenu />
         <div className="flex-1 shadow-2xl rounded-2xl">
-          {searchParams.get("section") === "public_profile" && <Information />}
-          {searchParams.get("section") === "account_settings" && (
-            <AccountSetting />
+          {searchParams.get("section") === "public_profile" && (
+            <Information key={"information"} />
           )}
-          {searchParams.get("section") === "your_resumes" && <Resume />}
+          {searchParams.get("section") === "account_settings" && (
+            <AccountSetting key={"account_setting"} />
+          )}
+          {searchParams.get("section") === "your_resumes" && (
+            <Resume key={"resumes"} />
+          )}
         </div>
       </div>
     </Page>
