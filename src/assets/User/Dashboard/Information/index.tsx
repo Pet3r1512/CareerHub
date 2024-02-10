27af1e1fdf06e7f14c7ad21b1256eefdf6963b2e
Thanh Phong <pttp15122002@gmail.com>
2024-02-10 10:43:15 +0700

    fixbug

---
+++ src/assets/User/Dashboard/Information/index.tsx
@@ -0,0 +1,20 @@
+import UserDashboardSectionLayout from "../_UserDashboardSectionLayout";
+import DataForm from "./dataForm";
+
+function Information() {
+  return (
+    <UserDashboardSectionLayout sectionTitle="Information">
+      <div>
+        <p className="text-gray-dark">
+          You can choose not to complete the following information, but if you
+          decide to apply for a job through CareerHub, we need you to provide
+          the following information to complete the application process for
+          companies.
+        </p>
+        <DataForm />
+      </div>
+    </UserDashboardSectionLayout>
+  );
+}
+
+export default Information;
