830b6f17ec5c177f954e60c9538afeb47ee72846
Thanh Phong <pttp15122002@gmail.com>
2024-02-12 13:47:54 +0700

    add styling

--- src/assets/User/Dashboard/Information/index.tsx
+++ src/assets/User/Dashboard/Information/index.tsx
@@ -4,7 +4,7 @@ import DataForm from "./dataForm";
 function Information() {
   return (
     <UserDashboardSectionLayout sectionTitle="Information">
-      <div>
+      <div className="flex flex-col gap-y-6">
         <p className="text-gray-dark">
           You can choose not to complete the following information, but if you
           decide to apply for a job through CareerHub, we need you to provide
