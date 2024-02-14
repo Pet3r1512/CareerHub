1745741cfb4f6178ca213c7976e8b2c049c01d44
Thanh Phong <pttp15122002@gmail.com>
2024-02-14 14:30:36 +0700

    refactor UI

--- src/assets/User/Dashboard/Information/index.tsx
+++ src/assets/User/Dashboard/Information/index.tsx
@@ -1,17 +1,24 @@
 import UserDashboardSectionLayout from "../_UserDashboardSectionLayout";
+import AdditionalDataForm from "./additionalDataForm";
 import DataForm from "./dataForm";
 
 function Information() {
   return (
     <UserDashboardSectionLayout sectionTitle="Information">
-      <div className="flex flex-col gap-y-6">
+      <div className="flex flex-col gap-y-4">
         <p className="text-gray-dark">
           You can choose not to complete the following information, but if you
           decide to apply for a job through CareerHub, we need you to provide
           the following information to complete the application process for
           companies.
         </p>
-        <DataForm />
+        <div className="flex flex-col gap-y-8">
+          <DataForm />
+          <AdditionalDataForm />
+        </div>
+        <p className="text-gray-dark text-right cursor-default">
+          ***This information is updated only once a month.
+        </p>
       </div>
     </UserDashboardSectionLayout>
   );
