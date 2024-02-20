9d2f330518b12db5f3fd90fac9e632471430b979
LeKhoi <ledinhkhoimd@gmail.com>
2024-02-20 20:11:12 +0700

    Update: add data for side option in job page

--- src/data/options.ts
+++ src/data/options.ts
@@ -75,3 +75,103 @@ export const companySize = [
     count: 23,
   },
 ];
+
+export const employmentType = [
+  {
+    type: "Full-time",
+    count: 3,
+  },
+  {
+    type: "Part-time",
+    count: 5,
+  },
+  {
+    type: "Remote",
+    count: 2,
+  },
+  {
+    type: "Internship",
+    count: 24,
+  },
+  {
+    type: "Contract",
+    count: 3,
+  },
+];
+
+export const jobCategories = [
+  {
+    category: "Design",
+    count: 24,
+  },
+  {
+    category: "Sales",
+    count: 3,
+  },
+  {
+    category: "Marketing",
+    count: 3,
+  },
+  {
+    category: "Business",
+    count: 3,
+  },
+  {
+    category: "Human Resources",
+    count: 6,
+  },
+  {
+    category: "Finance",
+    count: 4,
+  },
+  {
+    category: "Engineering",
+    count: 4,
+  },
+  {
+    category: "Technology",
+    count: 5,
+  },
+];
+
+export const jobLevel = [
+  {
+    level: "Entry Level",
+    count: 57,
+  },
+  {
+    level: "Mid Level",
+    count: 3,
+  },
+  {
+    level: "Senior Level",
+    count: 5,
+  },
+  {
+    level: "Director",
+    count: 12,
+  },
+  {
+    level: "VP and Above",
+    count: 8,
+  },
+];
+
+export const salaryRange = [
+  {
+    range: "$700 - $1000",
+    count: 4,
+  },
+  {
+    range: "$1000 - $1500",
+    count: 6,
+  },
+  {
+    range: "$1500 - $2000",
+    count: 10,
+  },
+  {
+    range: "$3000 or above",
+    count: 4,
+  },
+];
