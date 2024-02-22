2b8b47d61f9ff76a20280135145994f7f0006ad0
Thanh Phong <pttp15122002@gmail.com>
2024-02-22 09:10:29 +0700

    Add: Blog info in-short

---
+++ src/assets/Advice/blogContainer.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import { Blog } from "./blog";
+import Link from "next/link";
+
+export default function BlogContainer({ blog }: { blog: Blog }) {
+  const imgStyle = {
+    width: "auto",
+  };
+
+  return (
+    <Link href={"/blogs?blog=" + blog.id.toString()}>
+      <div className="w-96 shadow-xl rounded-2xl p-2 cursor-default lg:hover:shadow-2xl">
+        <Image
+          src={blog.img_url}
+          className="object-cover rounded-xl"
+          alt={""}
+          height={24}
+          width={1000}
+          quality={20}
+          style={imgStyle}
+        />
+        <h1 className="line-clamp-2 font-semibold text-lg">{blog.title}</h1>
+        <p className="text-right">{blog.published_date}</p>
+      </div>
+    </Link>
+  );
+}
