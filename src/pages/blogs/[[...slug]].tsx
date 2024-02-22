f5be3e9ea88a2afa27bd74dc9b79e82d5cfbd6cc
Thanh Phong <pttp15122002@gmail.com>
2024-02-22 15:03:49 +0700

    Optimize: get blog detail by useEffect, loading state with skeleton during loading

--- src/pages/blogs/[[...slug]].tsx
+++ src/pages/blogs/[[...slug]].tsx
@@ -1,19 +1,40 @@
 import { useSearchParams } from "next/navigation";
 
-import { coverLetters } from "@/data/advices/cover-letters";
-import Blog from "@/assets/Advice/blog";
+import { advices_blogs } from "@/data/advices/advice-blogs";
+import Blog, { BlogProps } from "@/assets/Advice/blog";
 import Page from "@/assets/_UI/Page";
+import { useEffect, useState } from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default function BlogPage() {
+export default function BlogDetail() {
+  const [currentBlog, setCurrentBlog] = useState<BlogProps>();
+  const [loading, setLoading] = useState(false);
   const blogId = useSearchParams().get("blog");
+
+  function getBlogDetail(blogId: string) {
+    return advices_blogs.find((item) => item.id.toString() === blogId);
+  }
+
+  useEffect(() => {
+    setLoading(true);
+    const timer = setTimeout(() => {
+      setCurrentBlog(getBlogDetail(blogId!));
+      setLoading(false);
+    }, Math.random() * (1500 - 500) + 500);
+    return () => clearTimeout(timer);
+  }, [useSearchParams()]);
+
   return (
     <Page pageName="Blog">
       <div className="pt-16">
-        {coverLetters.map((item) => {
-          if (item.id.toString() === blogId) {
-            return <Blog key={item.id} blog={item} />;
-          }
-        })}
+        {loading ? (
+          <>
+            <Skeleton className="w-full h-[336px]" />
+            <Skeleton className="w-full h-[31.5px] mt-4" />
+          </>
+        ) : (
+          currentBlog && <Blog blog={currentBlog} />
+        )}
       </div>
     </Page>
   );
