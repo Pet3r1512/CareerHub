c880d42a81e7a8556e95eaad1483d64783ebd2f0
Thanh Phong <pttp15122002@gmail.com>
2024-02-22 16:42:15 +0700

    Add: new blog

--- src/data/advices/advice-blogs.ts
+++ src/data/advices/advice-blogs.ts
@@ -4,7 +4,7 @@ export const advices_blogs = [
     img_url:
       "https://images.unsplash.com/photo-1501504905252-473c47e087f8?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     author: "CareerHub",
-    type: "Resume",
+    type: "Resume Tips",
     published_date: "22/02/2024",
     title:
       "Crafting Compelling Cover Letters: Your Ultimate Guide to Captivating Employers",
@@ -50,7 +50,60 @@ export const advices_blogs = [
     img_url:
       "https://images.unsplash.com/photo-1573497491208-6b1acb260507?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW50ZXJ2aWV3fGVufDB8MHwwfHx8MA%3D%3D",
     author: "ChatGPT",
-    type: "Interview",
+    type: "Interview Preparation",
+    published_date: "22/02/2024",
+    title:
+      "Ace Your Next Interview: Essential Tips for Preparation and Success",
+    content: `<div class="p-4 bg-white rounded-lg shadow-md mb-8">
+      <p class="text-xl mb-4>Introduction:</p>
+      <p class="text-lg mb-8">
+      Congratulations! You've landed an interview for a job you're excited about. Now it's time to prepare thoroughly to make a lasting impression on your potential employer. In this guide, we'll share essential tips to help you prepare for your interview effectively and increase your chances of success.
+      </p>
+      <ol class="list-decimal pl-4">
+        <li class="mb-4">
+          <h3 class="text-lg font-semibold mb-2">1. Research the Company and Role:</h3>
+          <ol>- Gather information about the company's mission, values, culture, and recent news.</ol>
+          <ol>- Understand the job requirements and responsibilities to tailor your responses accordingly.</ol>
+        </li>
+        <li class="mb-4">
+          <h3 class="text-lg font-semibold mb-2">2. Practice Common Interview Questions:</h3>
+          <ol>- Review common interview questions and prepare concise, thoughtful responses.</ol>
+          <ol>- Practice answering behavioral interview questions using the STAR method (Situation, Task, Action, Result).</ol>
+        </li>
+        <li class="mb-4">
+          <h3 class="text-lg font-semibold mb-2">3. Showcase Your Skills and Experiences:</h3>
+          <ol>- Prepare examples and anecdotes that demonstrate your relevant skills and experiences.</ol>
+          <ol>- Quantify your achievements whenever possible to provide concrete evidence of your capabilities.</ol>
+        </li>
+        <li class="mb-4">
+          <h3 class="text-lg font-semibold mb-2">4. Dress Appropriately and Arrive Early:</h3>
+          <ol>- Choose professional attire that aligns with the company's dress code and culture.</ol>
+          <ol>- Plan your route and aim to arrive at least 15 minutes early to account for any unexpected delays./ol>
+        </li>
+        <li class="mb-4">
+          <h3 class="text-lg font-semibold mb-2">5. Demonstrate Enthusiasm and Confidence:
+          </h3>
+          <ol>- Show enthusiasm for the role and company throughout the interview.</ol>
+          <ol>- Maintain good eye contact, posture, and body language to convey confidence and professionalism.</ol>
+        </li>
+        <li class="mb-4">
+          <h3 class="text-lg font-semibold mb-2">6. Prepare Questions for the Interviewer:
+          </h3>
+          <ol>- Prepare insightful questions to ask the interviewer about the role, team, and company culture.</ol>
+          <ol>- Demonstrate your interest and engagement in the opportunity by asking thoughtful questions.</ol>
+        </li>
+      </ol>
+      <p class="text-lg font-bold"Conclusion:
+      By following these tips and investing time in thorough preparation, you'll position yourself as a strong candidate and increase your chances of success in your next interview. Remember to be authentic, confident, and enthusiastic, and let your passion for the role shine through. Good luck!</p>
+    </div>
+        `,
+  },
+  {
+    id: 2202202403,
+    img_url:
+      "https://plus.unsplash.com/premium_photo-1678565869434-c81195861939?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8Y29tcHV0ZXIlMjBzY2llbmNlfGVufDB8MHwwfHx8MA%3D%3D",
+    author: "Peter Pham",
+    type: "Personal Branding",
     published_date: "22/02/2024",
     title:
       "Ace Your Next Interview: Essential Tips for Preparation and Success",
