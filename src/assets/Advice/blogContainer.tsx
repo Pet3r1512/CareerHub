cd498470e9502d29e87b20ed708faba241c11a09
Thanh Phong <pttp15122002@gmail.com>
2024-02-22 17:10:55 +0700

    Revert "Make: add spacing for mobiles and tablets"
    This reverts commit 8e7ec6e20ae1803f736c53fe6a9e677b438313e7.

--- src/assets/Advice/blogContainer.tsx
+++ src/assets/Advice/blogContainer.tsx
@@ -13,7 +13,7 @@ export default function BlogContainer({ blog }: { blog: BlogProps }) {
       href={"/blogs?blog=" + blog.id.toString()}
       className="inline-block w-fit lg:w-full mx-auto lg:mx-0 sm:h-[320px] lg:h-[355px]"
     >
-      <div className="w-96 lg:w-[420px] sm:h-[320px] lg:h-[355px] shadow-xl rounded-2xl py-2 cursor-default lg:hover:shadow-2xl px-4 flex gap-y-4 lg:gap-0 flex-col justify-between">
+      <div className="w-96 lg:w-[420px] sm:h-[320px] lg:h-[355px] shadow-xl rounded-2xl py-2 cursor-default lg:hover:shadow-2xl px-4 flex flex-col justify-between">
         <Image
           src={blog.img_url}
           className="object-contain rounded-xl lg:h-[245px] lg:w-full mx-auto"
