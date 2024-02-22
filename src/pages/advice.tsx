9a89a93c7ba10f76f867f5329ae6e28a5bb3da71
Thanh Phong <pttp15122002@gmail.com>
2024-02-22 16:43:39 +0700

    Optimize: call API for blogs, handling errors and set loading state

--- src/pages/advice.tsx
+++ src/pages/advice.tsx
@@ -1,13 +1,35 @@
+import { BlogProps } from "@/assets/Advice/blog";
 import BlogContainer from "@/assets/Advice/blogContainer";
 import Page from "@/assets/_UI/Page";
 
 import { advices_blogs } from "@/data/advices/advice-blogs";
+import { useEffect, useState } from "react";
 
 export default function Advice() {
+  const [blogs, setBlogs] = useState<BlogProps[]>([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getBlogs = async () => {
+      try {
+        const result = await fetch("/api/blogs/getBlogs");
+        if (result.ok) {
+          const data = await result.json();
+          setBlogs(data.blogs);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getBlogs();
+    setLoading(false);
+  }, []);
+
   return (
     <Page pageName="Advice">
-      <div className="pt-16">
-        {advices_blogs.map((blog, index) => {
+      <div className="pt-16 grid lg:grid-cols-3 grid-cols-1 sm:grid-cols-2 gap-6 sm:gap-y-14 lg:gap-0">
+        {blogs?.map((blog, index) => {
           return <BlogContainer key={blog.id + index.toString()} blog={blog} />;
         })}
       </div>
