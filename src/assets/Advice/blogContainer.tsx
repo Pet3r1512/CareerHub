321b2e8ee6fb6056ecbbc5c15294dc62732b1b8b
Thanh Phong <89619124+Pet3r1512@users.noreply.github.com>
2024-02-23 10:58:36 +0700

    Dev ver 1.1 (#43)
    * Bump: update web app version
    
    * Add: unsplash domains
    
    * Add: sample blogs
    
    * Start: Advice page
    
    * Add: blog detail layout
    
    * Make: update sections' href
    
    * Add: Blog info in-short
    
    * Add: render blog's detail by id from URL query
    
    * Fix: missing keys
    
    * Update: blogs file's name
    
    * Optimize: get blog detail by useEffect, loading state with skeleton during loading
    
    * Refactor: update interface's name and adding margin for top only
    
    * Optimize: use new interface name and add priority for next/image
    
    * Refactor: use new name
    
    * Add: shadcn skeleton
    
    * Add: new blogs
    
    * Refactor: apply fixed height to make all containers on the same line
    
    * Add: unsplash domain
    
    * Add: Blog type
    
    * Refactor: UI and add badge for blog type
    
    * Add: new blog
    
    * Add: get all blogs API
    
    * Optimize: call API for blogs, handling errors and set loading state
    
    * Add: blog container skeleton layout
    
    * Optimize: use skeleton while loading
    
    * Make: add spacing for mobiles and tablets
    
    * Make: change page name from advice to blogs
    
    * push
    
    * Revert "Make: add spacing for mobiles and tablets"
    
    This reverts commit 8e7ec6e20ae1803f736c53fe6a9e677b438313e7.
    
    * Fix: section name
    
    * Fix: UI bugs
    
    * Add: new blog
    
    * Refactor: reverse the blogs arrays so the latest will be placed first
    
    * Add: display "today" if the published_date is today

--- src/assets/Advice/blogContainer.tsx
+++ src/assets/Advice/blogContainer.tsx
@@ -8,15 +8,33 @@ export default function BlogContainer({ blog }: { blog: BlogProps }) {
     width: "auto",
   };
 
+  function isToday(dateString: string): string {
+    const currentDate: Date = new Date();
+    const currentDateString: string = currentDate.toLocaleDateString("en-GB");
+
+    const parts: string[] = dateString.split("/");
+    const dateFromDateString: Date = new Date(
+      parseInt(parts[2]),
+      parseInt(parts[1]) - 1,
+      parseInt(parts[0])
+    );
+
+    if (currentDate.toDateString() === dateFromDateString.toDateString()) {
+      return "Today";
+    } else {
+      return dateString;
+    }
+  }
+
   return (
     <Link
       href={"/blogs?blog=" + blog.id.toString()}
-      className="inline-block w-fit lg:w-full mx-auto lg:mx-0 sm:h-[320px] lg:h-[355px]"
+      className="inline-block w-fit lg:w-full mx-auto lg:mx-0 sm:h-[335px] lg:h-[370px]"
     >
-      <div className="w-96 lg:w-[420px] sm:h-[320px] lg:h-[355px] shadow-xl rounded-2xl py-2 cursor-default lg:hover:shadow-2xl px-4 flex flex-col justify-between">
+      <div className="w-96 xl:w-[435px] sm:h-[335px] lg:h-[370px] shadow-xl rounded-2xl py-2 cursor-default lg:hover:shadow-2xl px-4 pb-4 flex flex-col justify-between gap-y-4 sm:gap-y-0">
         <Image
           src={blog.img_url}
-          className="object-contain rounded-xl lg:h-[245px] lg:w-full mx-auto"
+          className="object-contain rounded-xl h-1/2 lg:h-[245px] lg:w-full mx-auto"
           alt={""}
           height={1000}
           width={1000}
@@ -27,7 +45,9 @@ export default function BlogContainer({ blog }: { blog: BlogProps }) {
         <h1 className="line-clamp-2 font-semibold text-lg">{blog.title}</h1>
         <div className="flex items-center justify-between">
           <Badge className="bg-primary text-white">{blog.type}</Badge>
-          <p className="text-right">{blog.published_date}</p>
+          <p className="text-right font-semibold">
+            {isToday(blog.published_date)}
+          </p>
         </div>
       </div>
     </Link>
