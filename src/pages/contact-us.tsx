2b34ff156fb2a3d5233c74157ab3d83c5d317783
Min2Solid <mintodalee@gmail.com>
2024-02-27 16:56:17 +0700

    build our team page

---
+++ src/pages/contact-us.tsx
@@ -0,0 +1,88 @@
+import Page from "@/assets/_UI/Page"
+
+export default function ContactUs(){
+    return(
+        <Page pageName="Contact Us">
+            <div className="py-20 bg-white">
+                <div className="container mx-auto px-6 md:px-12 lg:px-8">
+                    <div className="mb-12 space-y-4 text-center">
+                        <h1 className="text-4xl font-bold text-blue md:text-5xl">Our Team</h1>
+                        <p className="text-gray-600 lg:w-8/12 lg:mx-auto">This is our technical team. If you have any advice, please contact us with our infomation below</p>
+                    </div>
+
+                    <div className="py-20 gap-28 md:gap-48 flex justify-center relative">
+                        <p className="bg-gray-300 h-1 w-full absolute"></p>
+                        <div className="border-t-4 space-y-8 group text-center">
+                            <div className="w-32 h-32 -mt-16 mx-auto rounded-[2rem] rotate-45 overflow-hidden">
+                                <img src="https://media.discordapp.net/attachments/817598810883883028/1211973714498035772/IMG_0463.jpg?ex=65f0257a&is=65ddb07a&hm=73297ddf685f9720bff69add93a8feb6612ed34c5431489dba554c73f8aa1692&=&format=webp&width=526&height=701" alt="man picture" loading="lazy" width="640" height="805" className="w-full h-full object-cover -rotate-45 scale-125 mx-auto trasition duration-300 group-hover:scale-[1.4]"/>
+                            </div>
+                            <div className="text-center">
+                                <h5 className="text-xl text-gray-800 font-semibold">Phong Pham</h5>
+                                <span className="text-sm text-gray-500">CEO Fouder</span>
+                            </div>
+                            <a href="#" className="block w-max mx-auto text-center text-blue-500">View bio</a>
+                        </div>
+
+                        <div className="border-t-4 space-y-8 group text-center">
+                            <div className="w-32 h-32 -mt-16 mx-auto rounded-[2rem] rotate-45 overflow-hidden">
+                                <img src="https://scontent.fsgn5-15.fna.fbcdn.net/v/t1.6435-9/48415932_2361773440717657_172093043128664064_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=be3454&_nc_ohc=no0Efw1IA2QAX8WyHUz&_nc_ht=scontent.fsgn5-15.fna&oh=00_AfAFU8P47vb-rr6FklIjYHMlVXBpWH_w34owSxsjvG9Zsg&oe=66052356" alt="man picture" loading="lazy" width="640" height="805" className="w-full h-full object-cover -rotate-45 scale-125 mx-auto trasition duration-300 group-hover:scale-[1.4]"/>
+                            </div>
+                            <div className="text-center">
+                                <h5 className="text-xl text-gray-800 font-semibold">Le Khoi</h5>
+                                <span className="text-sm text-gray-500">CTO</span>
+                            </div>
+                            <a href="#" className="block w-max mx-auto text-center text-blue-500">View bio</a>
+                        </div>
+
+                        <div className="border-t-4 space-y-8 group text-center">
+                            <div className="w-32 h-32 -mt-16 mx-auto rounded-[2rem] rotate-45 overflow-hidden">
+                                <img src="https://scontent.fsgn5-9.fna.fbcdn.net/v/t1.6435-9/148016681_2937692559851159_287354116095940120_n.jpg?stp=dst-jpg_p526x296&_nc_cat=102&ccb=1-7&_nc_sid=dd63ad&_nc_ohc=rMPjNbTeQLgAX__GJ9I&_nc_ht=scontent.fsgn5-9.fna&oh=00_AfD47WD2GI5abw3AE5dRDaGrzVPIIbnwAtn8Crc4M5NoHw&oe=660503BE" alt="man picture" loading="lazy" width="640" height="805" className="w-full h-full object-cover -rotate-45 scale-125 mx-auto trasition duration-300 group-hover:scale-[1.4]"/>
+                            </div>
+                            <div className="text-center">
+                                <h5 className="text-xl text-gray-800 font-semibold">Linh Dan</h5>
+                                <span className="text-sm text-gray-500">CFO</span>
+                            </div>
+                            <a href="#" className="block w-max mx-auto text-center text-blue-500">View bio</a>
+                        </div>
+                    </div>
+
+                    <div className="py-20 gap-28 md:gap-48 flex justify-center relative">
+                        <p className="bg-gray-300 h-1 w-full absolute"></p>
+                        <div className="border-t-4 space-y-8 group text-center">
+                            <div className="w-32 h-32 -mt-16 mx-auto rounded-[2rem] rotate-45 overflow-hidden">
+                                <img src="https://scontent.fsgn5-14.fna.fbcdn.net/v/t39.30808-6/306611015_107373962122040_6885802368151341792_n.jpg?stp=cp6_dst-jpg&_nc_cat=101&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=wShRiOTyYE0AX9g_rh8&_nc_ht=scontent.fsgn5-14.fna&oh=00_AfAHFcxWngcLeyj7P01oqmZJDhmnN6JwwA9RM8QAavMi4A&oe=65E31C90" alt="man picture" loading="lazy" width="640" height="805" className="w-full h-full object-cover -rotate-45 scale-125 mx-auto trasition duration-300 group-hover:scale-[1.4]"/>
+                            </div>
+                            <div className="text-center">
+                                <h5 className="text-xl text-gray-800 font-semibold">Minh Le</h5>
+                                <span className="text-sm text-gray-500">Developer</span>
+                            </div>
+                            <a href="#" className="block w-max mx-auto text-center text-blue-500">View bio</a>
+                        </div>
+
+                        <div className="border-t-4 space-y-8 group text-center">
+                            <div className="w-32 h-32 -mt-16 mx-auto rounded-[2rem] rotate-45 overflow-hidden">
+                                <img src="https://cdn.discordapp.com/attachments/817598810883883028/1211969571704016906/1672496769910108.JPG?ex=65f0219f&is=65ddac9f&hm=6aad7d0e0ea1efdada86488a5342f05dbaf1dbebeb635f80a933fac6133a6f61&" alt="man picture" loading="lazy" width="640" height="805" className="w-full h-full object-cover -rotate-45 scale-125 mx-auto trasition duration-300 group-hover:scale-[1.4]"/>
+                            </div>
+                            <div className="text-center">
+                                <h5 className="text-xl text-gray-800 font-semibold">Nhat Khang</h5>
+                                <span className="text-sm text-gray-500">Developer</span>
+                            </div>
+                            <a href="#" className="block w-max mx-auto text-center text-blue-500">View bio</a>
+                        </div>
+
+                        <div className="border-t-4 space-y-8 group text-center">
+                            <div className="w-32 h-32 -mt-16 mx-auto rounded-[2rem] rotate-45 overflow-hidden">
+                                <img src="https://scontent.fsgn5-11.fna.fbcdn.net/v/t39.30808-6/319898123_958994875075649_8259774059601586265_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=9c7eae&_nc_ohc=ZLD8dOqh71sAX-ObPUJ&_nc_ht=scontent.fsgn5-11.fna&oh=00_AfCc0dJhDUlS1TnUJDDQ0Y0PZfULLclrm8gi_dg15Xa_7A&oe=65E285CC" alt="man picture" loading="lazy" width="640" height="805" className="w-full h-full object-cover -rotate-45 scale-125 mx-auto trasition duration-300 group-hover:scale-[1.4]"/>
+                            </div>
+                            <div className="text-center">
+                                <h5 className="text-xl text-gray-800 font-semibold">Ben Vu</h5>
+                                <span className="text-sm text-gray-500">Devoloper</span>
+                            </div>
+                            <a href="#" className="block w-max mx-auto text-center text-blue-500">View bio</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </Page>
+    )
+}
