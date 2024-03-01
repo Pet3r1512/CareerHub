33945eccdb9b2787f9dfaeed7595b2f51c10546e
LeKhoi <ledinhkhoimd@gmail.com>
2024-03-01 09:21:35 +0700

    Start: create filter func on mobile devices

---
+++ src/components/filterDialog.tsx
@@ -0,0 +1,21 @@
+import { Filter } from "lucide-react";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
+export default function FilterDialog() {
+  return (
+    <Dialog>
+      <DialogTrigger>
+        <div>
+          <Filter />
+        </div>
+      </DialogTrigger>
+    </Dialog>
+  );
+}
