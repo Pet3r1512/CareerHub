757ad734dde3221a990f4d710fcfd73f5b071064
LeeDaMin <121472778+Min2Solid@users.noreply.github.com>
2024-03-01 16:44:32 +0700

    Build company info label (#60)
    * start: build template for each label
    
    * add: build company info labels
    
    * stop: use company label
    
    * Fix: key props

--- src/pages/companies/[slug].tsx
+++ src/pages/companies/[slug].tsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import { companies } from "@/data/companies";
 import Page from "@/assets/_UI/Page";
+import CompanyLabel from "@/components/company/company-label";
 
 export default function CompanyPage() {
   const router = useRouter();
@@ -16,5 +17,17 @@ export default function CompanyPage() {
     return <Page pageName="Not Found">Company not found</Page>;
   }
 
-  return <Page pageName={company.name}>{company.name}</Page>;
+  return (
+    <Page pageName={company.name}>
+      {company.name}
+      <CompanyLabel
+        content={{
+          founded: "23-02-2002",
+          employees: 6,
+          location: "Ho Chi Minh City",
+          industry: company.name,
+        }}
+      />
+    </Page>
+  );
 }
