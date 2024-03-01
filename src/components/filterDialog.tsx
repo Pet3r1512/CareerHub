461025fe6fd5a2a1b6d1ae1b8e5a92688c00c4bd
LeKhoi <ledinhkhoimd@gmail.com>
2024-03-01 09:45:26 +0700

    Add: complete filter dialog content

--- src/components/filterDialog.tsx
+++ src/components/filterDialog.tsx
@@ -2,20 +2,31 @@ import { Filter } from "lucide-react";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import React from "react";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
-export default function FilterDialog() {
+export default function FilterDialog({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <Dialog>
-      <DialogTrigger>
+      <DialogTrigger className="lg:hidden p-2 bg-gray-300 rounded-md text-primary/80">
         <div>
-          <Filter />
+          <Filter size={18} strokeWidth={2} />
         </div>
       </DialogTrigger>
+      <DialogContent className="rounded-md w-2/3 h-1/2">
+        <DialogHeader>
+          <DialogTitle>Filter</DialogTitle>
+        </DialogHeader>
+        <ScrollArea type="always">{children}</ScrollArea>
+      </DialogContent>
     </Dialog>
   );
 }
