d78b6959f385333e44cc09e97d4779be80436848
LeKhoi <ledinhkhoimd@gmail.com>
2024-03-02 13:43:37 +0700

    Start: build create organization page

---
+++ src/pages/business/create-organization.tsx
@@ -0,0 +1,11 @@
+import Page from "@/assets/Business/_UI/Page";
+
+export default function CreateOrganization() {
+  return (
+    <Page pageName="Create Organization">
+      <div className="flex items-center justify-center">
+        <h1>Create Organization</h1>
+      </div>
+    </Page>
+  );
+}
