6d443a96560a3bb2ed9cfaa30e7916466e7f712e
Phat <vuletanphat.7215.got@gmail.com>
2024-03-04 18:53:37 +0700

    add 404 error page

---
+++ src/pages/404.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+
+const NotFoundPage: React.FC = () => {
+  return (
+    
+    <div className="justify-center min-h-screen bg-contain bg-right flex items-center flex-col md:flex-row">
+        <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4 text-primary">{`404 - Not found`}</h1>
+        <p className="text-lg">{`Sorry, the page you are looking for does not exist.`}</p>
+        <p className="text-lg">{`Please go back to `}<Link href="http://localhost:3000/" className="text-primary underline" >Home</Link> </p>
+        </div>
+        <div className="h-[500px] relative w-[500px]">
+        <Image src="/images/Ilustration.png" alt="" fill className="object-cover"/>
+        </div>
+    
+    </div>
+  );
+};
+
+export default NotFoundPage;
