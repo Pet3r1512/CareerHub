0f0a197f45f2961d6e0ef023c0e48e570a84a388
Phat <vuletanphat.7215.got@gmail.com>
2024-03-05 14:57:50 +0700

    fix-404-2

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,21 +1,17 @@
 import Link from "next/link";
 import Image from "next/image";
 
-const NotFoundPage: React.FC = () => {
+export default function NotFoundPage() {
   return (
-    
     <div className="justify-center min-h-screen bg-contain bg-right flex items-center flex-col md:flex-row">
-        <div className="text-center">
+      <div className="text-center">
         <h1 className="text-4xl font-bold mb-4 text-primary">{`404 - Not found`}</h1>
         <p className="text-lg">{`Sorry, the page you are looking for does not exist.`}</p>
         <p className="text-lg">{`Please go back to `}<Link href="/" className="text-primary underline" >Home</Link> </p>
-        </div>
-        <div className="h-[500px] relative w-[500px]">
+      </div>
+      <div className="h-[500px] relative w-[500px]">
         <Image src="/images/Ilustration.png" alt="" fill className="object-cover"/>
-        </div>
-    
+      </div>
     </div>
   );
-};
-
-export default NotFoundPage;
+}
