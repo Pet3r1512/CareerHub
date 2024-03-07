6968e75fc0e4ef3ca34136e2b59d976d308a6b84
Thanh Phong <pttp15122002@gmail.com>
2024-03-07 11:19:06 +0700

    Add: admin authentication

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,10 +1,23 @@
 import Page from "@/assets/_UI/Page";
 import Header from "@/assets/admin/header";
+import { useEffect, useState } from "react";
+import { Signin } from "@/assets/admin/signin";
 
 export default function AdminPage() {
+  const [admin, setAdmin] = useState("");
+
+  useEffect(() => {
+    if (localStorage.getItem("admin")) {
+      return setAdmin(localStorage.getItem("admin")!);
+    }
+  }, []);
+
   return (
     <Page noHeader>
-      <Header />
+      <Header admin={admin} />
+      <section className="flex min-h-[80dvh] w-full items-center justify-center">
+        {admin === "" ? <Signin admin={admin} setAdmin={setAdmin} /> : <></>}
+      </section>
     </Page>
   );
 }
