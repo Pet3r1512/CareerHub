f464f9ef176678993a90aa8d9faaee4f0f55811b
Thanh Phong <pttp15122002@gmail.com>
2024-03-10 16:07:19 +0700

    Refactor: using SSR to prevent non-admin accessing to admin's dashboard

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -1,3 +1,30 @@
-export default function Dashboard() {
-  return <>Logged In</>;
-}
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+
+const Dashboard = () => {
+  const [isAdmin, setIsAdmin] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const userRole = localStorage.getItem("admin");
+    if (!userRole || userRole === undefined) {
+      // Redirect to admin signin page if not authenticated or not admin
+      router.replace("/admin");
+    }
+    setIsAdmin(true);
+  }, []);
+
+  if (!isAdmin) {
+    // Return null or loading indicator while checking user role
+    return null;
+  }
+
+  return (
+    <div>
+      <h1>Admin Dashboard</h1>
+      <p>Admin logged in</p>
+    </div>
+  );
+};
+
+export default Dashboard;
