40115d946f18f1d281d20bce087ea6395fa3f834
LeKhoi <ledinhkhoimd@gmail.com>
2024-03-12 15:24:28 +0700

    Add: industry select field

---
+++ src/components/organization-form/industry-select.tsx
@@ -0,0 +1,75 @@
+"use client";
+import Select from "react-select";
+import { useFormContext } from "react-hook-form";
+
+const options = [
+  {
+    label: "Advertising",
+    value: "Advertising",
+  },
+  {
+    label: "Business Services",
+    value: "Business Services",
+  },
+  {
+    label: "Blockchain",
+    value: "Blockchain",
+  },
+  {
+    label: "Cloud",
+    value: "Cloud",
+  },
+  {
+    label: "Consumer Tech",
+    value: "Consumer Tech",
+  },
+  {
+    label: "Education",
+    value: "Education",
+  },
+  {
+    label: "Fintect",
+    value: "Fintech",
+  },
+  {
+    label: "Gaming",
+    value: "Gaming",
+  },
+  {
+    label: "Food & Beverage",
+    value: "Food & Beverage",
+  },
+  {
+    label: "Healthcare",
+    value: "Healthcare",
+  },
+  {
+    label: "Hosting",
+    value: "Hosting",
+  },
+  {
+    label: "Media",
+    value: "Media",
+  },
+];
+
+export default function IndustrySelect() {
+  const { setValue } = useFormContext();
+
+  return (
+    <Select
+      options={options}
+      isMulti
+      instanceId={"industry_type"}
+      placeholder="Select Industry"
+      className="basic-multi-select text-sm text-gray-dark"
+      classNamePrefix="select"
+      onChange={(val) =>
+        setValue(
+          "industry_type",
+          val.map((c) => c.value)
+        )
+      }
+    />
+  );
+}
