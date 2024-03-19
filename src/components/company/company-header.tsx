f267c4999fea7d99cc8ee1a9efbe6c2d4a23c26a
Le Khoi <84971441+lekhoi2709@users.noreply.github.com>
2024-03-19 16:29:36 +0700

    Make: adjust ui (#88)

--- src/components/company/company-header.tsx
+++ src/components/company/company-header.tsx
@@ -20,23 +20,21 @@ export default function CompanyHeader({
   industry: string[];
 }) {
   return (
-    <div className="flex flex-col lg:flex-row justify-between items-center gap-8 lg:border-y-2 lg:py-8">
-      <div className="relative w-full flex justify-between p-2 items-center lg:items-start">
-        <div className="lg:w-[150px] lg:h-[150px] w-[80px] h-[80px] relative">
-          <Image src={image} alt={name} fill style={{ objectFit: "cover" }} />
-        </div>
-        <div className="flex gap-4">
-          <Button className="text-primary bg-white hover:bg-white gap-x-2">
-            <Eye />
-            <span className="hidden xl:block">Public View</span>
-          </Button>
-          <Button className="text-primary bg-white hover:bg-white border-2 border-primary gap-x-2">
-            <Settings />
-            <span className="hidden xl:block">Profile Settings</span>
-          </Button>
-        </div>
+    <div className="flex flex-wrap items-center justify-between lg:justify-start lg:flex-nowrap lg:items-start gap-8 lg:border-y-2 lg:py-8 min-w-full">
+      <div className="lg:w-[150px] lg:h-[150px] w-[80px] h-[80px] relative order-1">
+        <Image src={image} alt={name} fill style={{ objectFit: "cover" }} />
       </div>
-      <div className="flex flex-col justify-center w-full lg:w-2/3 gap-y-5 lg:absolute lg:left-[20%] px-2 lg:p-0">
+      <div className="flex gap-4 lg:order-last order-2">
+        <Button className="text-primary bg-white hover:bg-white gap-x-2">
+          <Eye />
+          <span className="hidden xl:block">Public View</span>
+        </Button>
+        <Button className="text-primary bg-white hover:bg-white border-2 border-primary gap-x-2">
+          <Settings />
+          <span className="hidden xl:block">Profile Settings</span>
+        </Button>
+      </div>
+      <div className="flex flex-col justify-center w-full lg:w-2/3 gap-y-5 px-2 lg:p-0 lg:order-2 order-last">
         <div className="flex w-full text-3xl lg:text-5xl justify-between">
           <p>{name}</p>
         </div>
