c88ca5a3af07bb6c8f8a8957a87372864e24038c
Min2Solid <mintodalee@gmail.com>
2024-03-20 15:44:37 +0700

    Add: Add job header

---
+++ src/pages/jobs/job-demo.tsx
@@ -0,0 +1,10 @@
+import JobHeader from "@/assets/Jobs/job-header";
+import Page from "@/assets/_UI/Page";
+
+export default function JobDemo(){
+    return(
+        <Page pageName="Job Description">
+            <JobHeader></JobHeader>
+        </Page>
+    )
+}
