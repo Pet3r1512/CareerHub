77fb361128c37c7a67fc00d653d18c1fea2ebaa3
PhatBen234 <158719289+PhatBen234@users.noreply.github.com>
2024-03-20 17:35:39 +0700

    Build companies sponsors (#92)
    * Add: sponsor logo
    
    * Add: sponsor component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,14 @@ import Page from "@/assets/_UI/Page";
 import Category from "@/assets/home/Category";
 import FeaturedJobs from "@/assets/home/Featured-Jobs";
 import LatestJobs from "@/assets/home/Latest-Jobs";
+import Sponsor from "@/assets/home/sponsor";
 
 export default function Home() {
   return (
     <Page className="flex flex-col gap-y-4 md:gap-y-24">
       <section className="lg:min-h-screen flex flex-col lg:gap-24 gap-12">
         <Hero />
+        <Sponsor/>
         <Category />
         <FeaturedJobs />
         <LatestJobs />
