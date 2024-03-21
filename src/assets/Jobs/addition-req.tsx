e1c1b557de9ef249dc79123ab0c6e2043bd5fcc3
Min2Solid <mintodalee@gmail.com>
2024-03-21 16:16:54 +0700

    Build: build addition requirement

---
+++ src/assets/Jobs/addition-req.tsx
@@ -0,0 +1,23 @@
+import { CheckCircle2 } from "lucide-react";
+import Content from "./content";
+
+export default function AdditionReq() {
+  const Nicetohave = [
+    "Fluent in English",
+    "Project management skills",
+    "Copy editing skills",
+  ];
+
+  return (
+    <div className="flex flex-col gap-10">
+      <div className="flex flex-col gap-4">
+        <h1 className="text-2xl">Nice-To-Have</h1>
+        {Nicetohave.map((item, index) => {
+          return (
+            <Content item={item}/>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
