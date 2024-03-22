0438589f90f7b95f1d891e11527062972235f1ef
lekhoi2709 <ledinhkhoimd@gmail.com>
2024-03-22 15:40:10 +0700

    Add: container style for job capacity component

--- src/components/job/job-capacity-progress.tsx
+++ src/components/job/job-capacity-progress.tsx
@@ -5,22 +5,33 @@ import { Progress } from "../ui/progress";
 export default function JobCapacityProgress({
   applied,
   capacity,
+  containerClassName,
   className,
   isButton = true,
 }: {
   applied: number;
   capacity: number;
+  containerClassName?: string;
   className?: string;
   isButton?: boolean;
 }) {
   const progressValue = (applied * 100) / capacity;
   return (
-    <div className={twMerge("flex gap-4", className)}>
-      <Button className={twMerge("text-white px-8", isButton == true?'':'hidden')}>Apply</Button>
-      <div className="flex flex-col items-center justify-center gap-2 text-sm">
+    <div className={twMerge("flex gap-4", containerClassName)}>
+      <Button
+        className={twMerge("text-white px-8", isButton == true ? "" : "hidden")}
+      >
+        Apply
+      </Button>
+      <div
+        className={twMerge(
+          "flex flex-col items-center justify-center gap-2 text-sm",
+          className
+        )}
+      >
         <Progress
           value={progressValue}
-          className="h-2 bg-gray-300"
+          className="h-2 bg-gray-300 w-full"
           indicatorColor="bg-green"
         />
         <p>
