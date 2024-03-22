e96e5d446814569cd78f0d83b07d64eb8d75d71d
nhatkhang0910 <nhatkhang0910@gmail.com>
2024-03-22 14:27:42 +0700

    ok

---
+++ src/assets/User/Dashboard/PublicProfile/index.tsx
@@ -0,0 +1,9 @@
+import UserDashboardSectionLayout from "../_UserDashboardSectionLayout";
+
+export default function PublicProfile() {
+  return (
+    <UserDashboardSectionLayout sectionTitle="Public Profile">
+      <p></p> 
+    </UserDashboardSectionLayout> //hello
+  );
+}
